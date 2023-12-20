58f43f607d553b23868c296c2481d55e083649f4
shvmaksym <132988949+shvmaksym@users.noreply.github.com>
2023-12-20 13:04:21 +0200

    Fix previous commit

--- block.py
+++ block.py
@@ -6,8 +6,15 @@ class Block:
         self.id = id
         self.cells = {}
         self.cell_size = 50
+        self.offset_row = 0
+        self.offset_column = 0
         self.rotation = 0
         self.colors = Colors.get_cell_color()
+
+    def move(self, rows, columns):
+        self.offset_row += rows
+        self.offset_column += columns
+    
     
     def draw(self, screen):
         tiles = self.cells[self.rotation]
